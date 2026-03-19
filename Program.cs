294fcd6724543151d0b6fba98fb658935f865bdd
2014000002-AllanGonzalez <a.gonzalez.5ddb@gmail.com>
2026-03-19 12:12:17 -0600

    Add files via upload
    visual estudio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //entradas
+        Console.WriteLine("===Numeros Positivos y Negativos");
+        int num;
+        Console.WriteLine("Dame un numero entero: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        //procesos
+        //salidas
+        if (num >= 0)
+        {
+            Console.WriteLine("Es un numero positivo");
+        }
+        else
+        {
+            Console.WriteLine("Es un numero negativo");
+        }
+    }
+}
