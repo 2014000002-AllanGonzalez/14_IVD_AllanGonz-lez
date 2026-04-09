e62918051dccbc1919f6e35dfff985f7f91f0498
2014000002-AllanGonzalez <a.gonzalez.5ddb@gmail.com>
2026-04-09 11:37:42 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,20 +2,38 @@
 {
     private static void Main(string[] args)
     {
-        //entradas
-        Console.WriteLine("===Numeros Positivos y Negativos");
-        int num;
-        Console.WriteLine("Dame un numero entero: ");
-        num = Convert.ToInt32(Console.ReadLine());
-        //procesos
-        //salidas
-        if (num >= 0)
+        Console.WriteLine("******************");
+        Console.WriteLine("*Dia De La Semana*");
+        Console.WriteLine("******************");
+        Console.Write("Digite Un Numero del 1 al 7: ");
+        int dia=Convert.ToInt32(Console.ReadLine());
+
+        switch (dia)
         {
-            Console.WriteLine("Es un numero positivo");
-        }
-        else
-        {
-            Console.WriteLine("Es un numero negativo");
+            case 1:
+                Console.WriteLine("LUNES");
+                break;
+            case 2:
+                Console.WriteLine("MARTES");
+                break;
+            case 3:
+                Console.WriteLine("MIERCOLES");
+                break;
+            case 4:
+                Console.WriteLine("JUEVES");
+                break;
+            case 5:
+                Console.WriteLine("VIERNES");
+                break;
+            case 6:
+                Console.WriteLine("SABADO");
+                break;
+            case 7:
+                Console.WriteLine("DOMINGO");
+                break;
+            default:
+                Console.WriteLine("Digite un valor del 1 al 7");
+                break;
         }
     }
 }
